64c072da2de99e531518ca611806d66977afccc9
Ubuntu <ubuntu@ip-172-31-24-31.ec2.internal>
2025-02-10 06:40:07 +0000

    remove openai keys

--- fastapi_guardrails/app/config/config.py
+++ fastapi_guardrails/app/config/config.py
@@ -4,8 +4,6 @@ import os
 load_dotenv()
 
 class Config:
-    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-    
     DISABLE_CLASSIFIER = os.getenv('DISABLE_CLASSIFIER', 'False').lower() == 'False'
     DISABLE_LLM = os.getenv('DISABLE_LLM', 'False').lower() == 'False'
     SENSITIVE_TOPICS = os.getenv('SENSITIVE_TOPICS', '').split(',')
